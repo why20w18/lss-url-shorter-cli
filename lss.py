fafdc1d48a024e5f0fc4ad18f625ffb9eb4c451c
why20w18 <enco3162@gmail.com>
2024-05-11 00:48:01 +0300

    artik guncelleme yapabiliyor

--- lss.py
+++ lss.py
@@ -1,5 +1,6 @@
 import datetime
 import json
+import os
 import sys
 import requests
 import random
@@ -31,31 +32,31 @@ auzunluk = getJSON("ozellestirme","ayrac_uzunluk")
 
 yardim = rmav+ asembol*auzunluk + rbitir + f'''
 {ryes}YARDIM SAYFASI{rbitir} 
-    
-    TEMEL KOMUTLAR:
+    MEVCUT VERSIYON:{versiyon}
+    T EMEL KOMUTLAR:
     {rmav}lss{rbitir} {rkir}[URL]{rbitir}
     {rmav}lss{rbitir} {rkir}[URL]{rbitir} {rkir}[KAYNAK]{rbitir}
     
     TEMEL KULLANIMLAR:
-    {rmav}lss{rbitir} www.google.com    : cfg.json dosyasından rastgele API seçer
-    {rmav}lss{rbitir} www.google.com k1 : cfg.json dosyasındaki k1 API'sini seçer
+    {rmav}lss{rbitir} www.google.com         : cfg.json dosyasından rastgele API seçer
+    {rmav}lss{rbitir} www.google.com k1      : cfg.json dosyasındaki k1 API'sini seçer
     
     EK KOMUTLAR:
-    {rmav}lss{rbitir} {rkir}-h{rbitir}    : yardım sayfasını başlatır                      (help)
+    {rmav}lss{rbitir} {rkir}-h{rbitir}       : yardım sayfasını başlatır                      (help)
     
-    {rmav}lss{rbitir} {rkir}-v{rbitir}    : versiyon numarası sayfasını başlatır           (versiyon)
-    {rmav}lss{rbitir} {rkir}-uc{rbitir}   : güncelleme varmi kontrol eder                  (update-check)
-    {rmav}lss{rbitir} {rkir}-ua{rbitir}   : güncellemeyi otomatik olarak indirip kurar     (update-apply)
+    {rmav}lss{rbitir} {rkir}-v{rbitir}       : versiyon numarası sayfasını başlatır           (versiyon)
+    {rmav}lss{rbitir} {rkir}-uc{rbitir}      : güncelleme varmi kontrol eder                  (update-check)
+    {rmav}lss{rbitir} {rkir}-uc kur{rbitir}  : güncellemeyi otomatik olarak indirip kurar     (update-check-kur)
     
-    {rmav}lss{rbitir} {rkir}-g{rbitir}    : kısaltılan linkleri tarih damgası ile gösterir (geçmiş)
-    {rmav}lss{rbitir} {rkir}-gs{rbitir}   : kayıtların tutulduğu geçmişi siler             (geçmiş-sil)
+    {rmav}lss{rbitir} {rkir}-g{rbitir}       : kısaltılan linkleri tarih damgası ile gösterir (geçmiş)
+    {rmav}lss{rbitir} {rkir}-gs{rbitir}      : kayıtların tutulduğu geçmişi siler             (geçmiş-sil)
     
-    {rmav}lss{rbitir} {rkir}-pe{rbitir}   : programı otomatik olarak pathe ekler           (path-ekle)
+    {rmav}lss{rbitir} {rkir}-pe{rbitir}      : programı otomatik olarak pathe ekler           (path-ekle)
     
-    {rmav}lss{rbitir} {rkir}-ta{rbitir}   : tüm API'leri listeler                          (tüm-apiler)
+    {rmav}lss{rbitir} {rkir}-ta{rbitir}      : tüm API'leri listeler                          (tüm-apiler)
     
-    {rmav}lss{rbitir} {rkir}-a*{rbitir}   : API ve özelleştirmeler için ayarları başlatır  (ayarlar)
-    {rmav}lss{rbitir} {rkir}-ah{rbitir}   : ayarlar komutunun yardım sayfasını başlatır    (ayarlar-help)
+    {rmav}lss{rbitir} {rkir}-a*{rbitir}      : API ve özelleştirmeler için ayarları başlatır  (ayarlar)
+    {rmav}lss{rbitir} {rkir}-ah{rbitir}      : ayarlar komutunun yardım sayfasını başlatır    (ayarlar-help)
     
     https://github.com/\033[1;36mwhy20w18\033[0m
 '''+rmav+ asembol*auzunluk + rbitir
@@ -101,31 +102,46 @@ def tum_apiler():
 
 def iversiyon():
     print("lss versiyon : ",rmav,versiyon,rbitir,sep='')
+
 def guncellemeVarMi():
     #-uc argumani
     r = requests.get('https://api.github.com/repos/why20w18/lss-url-shorter-cli/releases/latest')
     guncel_release = r.json()
     guncel_versiyon = guncel_release["name"]
 
+    #print(sys.argv)
+
     indirme_link = guncel_release["assets"][0]["browser_download_url"]
     boyut = round(int(guncel_release["assets"][0]["size"]) / (2**20),2) #mb
 
+    guncelDegil = True
 
-    if versiyon != str(guncel_versiyon).strip():
-        print(ryes,"GUNCELLEME VAR !",rbitir+"\n"
-                                             "yeni versiyon: ",
-              rmav,str(guncel_versiyon).strip()[16:21],rbitir
-              ,sep='')
+    try:
+        if sys.argv[2] == "kur":
+            print("eski executable versiyon siliniyor ...")
+            os.system("rm -r lss")
+            print(rkir,"toplam indirme boyutu : ",boyut, "MB", rbitir+"\n",sep='')
+            subprocess.run(["wget", indirme_link])
+            print(rbitir,ryes+"\n\nwhy20w18","\bindirme tamamlandi", rbitir,sep='\n')
+            os.chmod("lss",0o755)
+            print(rmav,guncel_versiyon, "başlatılıyor !\n\n\n\n\nGIRILEN KOMUT:./lss -h\n",rbitir)
+            os.system("./lss -h")
+            print(rkir,"\n\n\nGUNCEL VERSIYONU KULLANMAK ICIN './lss -h' YAZIN",rbitir,sep='')
+            guncelDegil = False
 
-    elif sys.argv[1] == "-ua":
-        guncellemeyiKur(indirme_link,boyut)
+    except IndexError as hata:
+        pass
 
-    else:
-        print(ryes+"lls EN GUNCEL VERSIYONDA !",rbitir+
+    finally:
+        if guncelDegil and versiyon != str(guncel_versiyon).strip():(
+            print(ryes,"GUNCELLEME VAR !",rbitir
+                  +"\n"+"yeni versiyon: ",
+                  rmav,str(guncel_versiyon).strip()[16:21]
+                  ,rbitir,sep=''))
+
+        else:
+            print(ryes+"lls EN GUNCEL VERSIYONDA !",rbitir+
               "MEVCUT VERSIYON :" + guncel_versiyon,sep='\n')
-
-def guncellemeyiKur(indirme_link,boyut):
-    print(2)
 
 
 def main():
@@ -133,7 +149,6 @@ def main():
         "-h": lambda : print(yardim),
         "-v": iversiyon,
         "-uc": lambda : guncellemeVarMi(),
-        "-ua": guncellemeyiKur,
         "-g": gecmis_oku,
         "-gs": gecmis_sil,
         "-pe": lambda : print(2),
@@ -147,7 +162,7 @@ def main():
         print(rkir,'fazladan parametre girdiniz yardım için "-h"',rbitir,sep='')
 
     else:
-        if sys.argv[1] in ekKomutlar:
+        if sys.argv[1] or sys.argv[1] and sys.argv[2] in ekKomutlar:
             ekKomutlar[sys.argv[1]]()
         else:
             url = sys.argv[1]
